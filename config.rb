c33669fa58bd2454630883013c27189124b78565
Kristian Van Der Vliet <kvandervliet@dyn.com>
2013-12-18 16:04:50 +0000

    Add sample configuration

---
+++ config.rb
@@ -0,0 +1,7 @@
+########## Configuration block
+CHEF_SERVER_URL = "https://localhost"
+CHEF_CLIENT_NAME = "databag-api"
+CHEF_CLIENT_KEY = "./client.pem"
+
+API_BASE_URL = "https://localhost:8080/"
+########## End configuration block
